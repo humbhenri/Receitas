7c362224fc9c6af634f65db00a5b6f06b4eeb997
Humberto Henrique Campos Pinheiro <humberto.pinheiro@serpro.gov.br>
2025-12-03 15:24:12 -0300

    create test name empty

--- Tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ Tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,5 +1,6 @@
 using Commons.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
 using Shouldly;
 
 namespace Validators.Test.User.Register;
@@ -12,5 +13,17 @@ public class RegisterUserValidatorTest
         var validator = new RegisterUserValidator();
         var result = validator.Validate(RequestRegisterUserJsonBuilder.Build());
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem();
+        result.Errors.ShouldAllBe(e => e.ErrorMessage.Equals("Name cannot be empty"));
     }
 }
