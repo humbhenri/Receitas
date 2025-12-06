784652e1c1e82dd0fdbca0ee315feba5dfa5bc79
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2025-12-06 13:24:48 -0300

    lower case urls

--- Backend/MyRecipeBook.API/Program.cs
+++ Backend/MyRecipeBook.API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
-
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
