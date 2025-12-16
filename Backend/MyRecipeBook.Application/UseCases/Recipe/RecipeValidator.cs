e6bcb2e112fc983f3ea05923ac8ac8814d89aae8
Humberto Henrique Campos Pinheiro <humberto.pinheiro@serpro.gov.br>
2025-12-16 09:53:17 -0300

    recipe validator test

--- Backend/MyRecipeBook.Application/UseCases/Recipe/RecipeValidator.cs
+++ Backend/MyRecipeBook.Application/UseCases/Recipe/RecipeValidator.cs
@@ -11,5 +11,17 @@ public class RecipeValidator : AbstractValidator<RequestRecipeJson>
         RuleFor(r => r.Title).NotEmpty().WithMessage(Messages.recipe_empty_title);
         RuleFor(r => r.CookingTime).IsInEnum().WithMessage(Messages.cooking_time_not_supported);
         RuleFor(r => r.Difficulty).IsInEnum().WithMessage(Messages.difficulty_not_supported);
+        RuleFor(r => r.Ingredients.Count).GreaterThan(0).WithMessage(Messages.at_least_one_ingredient);
+        RuleFor(r => r.Instructions.Count).GreaterThan(0).WithMessage(Messages.at_least_one_instruction);
+        RuleForEach(r => r.DishTypes).IsInEnum().WithMessage(Messages.dish_type_not_supported);
+        RuleForEach(r => r.Ingredients).NotEmpty().WithMessage(Messages.ingredient_empty);
+        RuleForEach(r => r.Instructions).ChildRules(i =>
+        {
+            i.RuleFor(i => i.Step).GreaterThan(0).WithMessage(Messages.non_negative_step);
+            i.RuleFor(i => i.Text).NotEmpty().WithMessage(Messages.empty_instruction)
+                .MaximumLength(2000).WithMessage(Messages.instructions_too_big);
+        });
+        RuleFor(r => r.Instructions).Must(instructions => instructions.Select(i => i.Step).Distinct().Count() == instructions.Count)
+            .WithMessage(Messages.steps_must_be_unique);
     }
 }
--- Tests/Validators.Test/Recipe/RecipeValidatorTest.cs
+++ Tests/Validators.Test/Recipe/RecipeValidatorTest.cs
@@ -1,5 +1,6 @@
 using Commons.Requests;
 using MyRecipeBook.Application.UseCases.Recipe;
+using MyRecipeBook.Exceptions;
 using Shouldly;
 
 namespace Validators.Test.Recipe;
@@ -33,5 +34,37 @@ public class RecipeValidatorTest
         request.Difficulty = (MyRecipeBook.Communication.Enums.Difficulty?)100;
         var result = validator.Validate(request);
         result.IsValid.ShouldBe(false);
+    }
+
+    [Fact]
+    public void Success_DishTypes_Empty()
+    {
+        var validator = new RecipeValidator();
+        var request = RequestRecipeJsonBuilder.Build();
+        request.DishTypes.Clear();
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBe(true);
+    }
+
+    [Fact]
+    public void Error_Invalid_Dish_Types()
+    {
+        var validator = new RecipeValidator();
+        var request = RequestRecipeJsonBuilder.Build();
+        request.DishTypes.Clear();
+        request.DishTypes.Add((MyRecipeBook.Communication.Enums.DishType)1000);
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBe(false);
+    }
+
+    [Fact]
+    public void Error_Empty_Ingredients()
+    {
+        var validator = new RecipeValidator();
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Ingredients.Clear();
+        var result = validator.Validate(request);
+        result.IsValid.ShouldBe(false);
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(Messages.at_least_one_ingredient);
     }
 }
